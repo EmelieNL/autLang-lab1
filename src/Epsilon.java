5ebe1461556e0b27efb395328f36ac28d2061862
hejm <johanbergdorf@gmail.com>
2016-04-28 10:53:30 +0200

    Removed all epsilons except 1

--- src/Epsilon.java
+++ src/Epsilon.java
@@ -8,18 +8,4 @@ public class Epsilon {
 		return new String("Epsilon");
 	}
 	
-	@Override
-	public int hashCode()
-	{
-	    return this.hashCode();
-	}
-	
-	
-	@Override
-	public boolean equals(Object o)
-	{
-	    if (this.hashCode() == o.hashCode())return true;
-	    
-	    return false;
-	}
 }
